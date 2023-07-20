43f3e0c4f30a3964205075b8e6bbb9ecddb39237
s2Leuhan <88736303+s2Leuhan@users.noreply.github.com>
2023-07-20 14:25:11 -0300

    Update comprimir.sh

---
+++ comprimir.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function lista_imagenes() {
+  ls -1 *.jpg > EdP_lista_imagenes.txt
+}
+
+function lista_nombres_validos() {
+  grep -E '^[A-Z][a-z]' EdP_lista_imagenes.txt > EdP_lista_nombres_validos.txt
+}
+
+function total_nombres_con_a() {
+  grep -c -E '^*[Aa]\.jpg' EdP_lista_nombres_validos.txt > EdP_total_nombres_con_fin_a.txt
+}
+
+function comprimir_archivos() {
+  tar -czvf EdP_archivos_generados.tar.gz EdP_lista_imagenes.txt EdP_lista_nombres_validos.txt EdP_total_nombres_con_fin_a.txt *.jpg
+}
+
+lista_imagenes
+lista_nombres_validos
+total_nombres_con_a
+comprimir_archivos
+echo "Archivos generados y comprimidos exitosamente."
