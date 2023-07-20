92a9d562fb6c55371e3b9763476280b04c835e31
s2Leuhan <88736303+s2Leuhan@users.noreply.github.com>
2023-07-20 14:05:20 -0300

    Update generar.sh
    First release

--- generar.sh
+++ generar.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
 
+function descargar_imagenes() {
+  cantidad_imagenes=$1
+  nombres=($(curl -s https://raw.githubusercontent.com/adalessandro/EdP-2023-TP-Final/main/dict.csv | cut -d ',' -f1)) 
+  for ((i=1; i<=cantidad_imagenes; i++))
+  do
+    nombre_persona=${nombres[$RANDOM % ${#nombres[@]}]}
+    curl -s "https://thispersondoesnotexist.com/" -o "${nombre_persona}.jpg"
+    echo "Descargando imagen para: ${nombre_persona}..."
+    sleep 5
+  done
+}
+
+function comp_y_gen_checksum() {
+  tar -czvf imagenes.tar.gz *.jpg
+  md5sum imagenes.tar.gz > imagenes_checksum.txt
+}
+
+if [ $# == 1 ] # Checkeo el número de argumentos
+then
+  cantidad_imagenes=$1
+else
+  echo "Uso: ./generar.sh <cantidad_imagenes>"
+  exit 1
+fi
+
+descargar_imagenes $cantidad_imagenes
+comp_y_gen_checksum
